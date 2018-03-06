fb4b4cc74799f3d3e04f9dc8f962ec91f97cd19e
马苗苗 <mamiaomiao@mamiaomiaodeMacBook-Air.local>
2018-03-06 08:56:52 +0800

    test

---
+++ gulpfile.js
@@ -0,0 +1,4 @@
+var gulp = require("gulp");//引入本地安装的 gulp模块
+gulp.task("default",function(){//default 为默认任务名，这种情况只需要在命令行中输入 gulp即可。 如果有特定的taskName,需要在命令行中实行 gulp taskName
+　　console.log("hi, gulp")
+})
